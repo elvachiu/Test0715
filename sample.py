6cd7707b2f2b43448725f03419a7ced431331d3a
elvachiu <52899184+elvachiu@users.noreply.github.com>
2019-07-15 13:27:17 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -43,7 +43,7 @@ def handle_message(event):
     # 回應使用者輸入的話
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text=event.message.text))
 #    message = ImageSendMessage(original_content_url = 'http://example.com/original.jpg',
 #                           preview_image_url = 'https://example.com/preview.jpg')
 #    line_bot_api.reply_message(event.reply_token, message)
